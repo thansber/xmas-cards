30ffe1115524732ea6a8f32f4b92d01b2e3d2acb
thansber <thansber@yahoo.com>
2020-11-15 21:09:21 -0500

    copy favicon to dist dir when building

--- rollup.config.js
+++ rollup.config.js
@@ -1,7 +1,19 @@
 import { createDefaultConfig } from '@open-wc/building-rollup';
 
+import merge from 'deepmerge';
+import copy from 'rollup-plugin-copy';
+
 // if you need to support IE11 use "modern-and-legacy-config" instead.
 // import { createCompatibilityConfig } from '@open-wc/building-rollup';
 // export default createCompatibilityConfig({ input: './index.html' });
 
-export default createDefaultConfig({ input: './src/index.html' });
+const baseConfig = createDefaultConfig();
+
+export default merge(baseConfig, {
+  input: './src/index.html',
+  plugins: [
+    copy({
+      targets: [{ src: './src/favicon.ico', dest: 'dist' }],
+    }),
+  ],
+});

e7e2728a80757490816642e8bfa4978e51f07a7d
thansber <thansber@yahoo.com>
2020-12-08 17:25:56 -0500

    use updated code based on new version of rollup config

--- rollup.config.js
+++ rollup.config.js
@@ -1,13 +1,12 @@
-import { createDefaultConfig } from '@open-wc/building-rollup';
+import { createSpaConfig } from '@open-wc/building-rollup';
 
 import merge from 'deepmerge';
 import copy from 'rollup-plugin-copy';
 
-// if you need to support IE11 use "modern-and-legacy-config" instead.
-// import { createCompatibilityConfig } from '@open-wc/building-rollup';
-// export default createCompatibilityConfig({ input: './index.html' });
-
-const baseConfig = createDefaultConfig();
+const baseConfig = createSpaConfig({
+  developmentMode: process.env.ROLLUP_WATCH === 'true',
+  injectServiceWorker: false,
+});
 
 export default merge(baseConfig, {
   input: './src/index.html',
